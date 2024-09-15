ddf95dec78429c366cbd1f9ea76a9028045f5482
Alexander Dergunov <enselerizer@gmail.com>
2024-09-15 22:04:27 +0300

    #5

---
+++ esphome/components/custom_rfid_reader/text_sensor.py
@@ -0,0 +1,23 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart, text_sensor
+from esphome.const import ICON_EMPTY, UNIT_EMPTY
+
+DEPENDENCIES = ["uart"]
+
+custom_rfid_reader_ns = cg.esphome_ns.namespace("custom_rfid_reader")
+CustomRFIDReader = custom_rfid_reader_ns.class_(
+    "CustomRFIDReader", text_sensor.TextSensor, cg.Component, uart.UARTDevice
+)
+
+CONFIG_SCHEMA = (
+    text_sensor.text_sensor_schema(CustomRFIDReader)
+    .extend(uart.UART_DEVICE_SCHEMA)
+    .extend(cv.COMPONENT_SCHEMA)
+)
+
+
+async def to_code(config):
+    var = await text_sensor.new_text_sensor(config)
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
