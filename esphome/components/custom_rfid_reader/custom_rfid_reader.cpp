e78c154d098e4c193e8779b272ca5b2732f73978
Alexander Dergunov <enselerizer@gmail.com>
2024-09-15 22:27:27 +0300

    Setting up basic UART logic

--- esphome/components/custom_rfid_reader/custom_rfid_reader.cpp
+++ esphome/components/custom_rfid_reader/custom_rfid_reader.cpp
@@ -2,22 +2,72 @@
 #include "custom_rfid_reader.h"
 #include "esphome/core/log.h"
 
-namespace esphome {
-namespace custom_rfid_reader {
+namespace esphome
+{
+    namespace custom_rfid_reader
+    {
 
-static const char *TAG = "custom_rfid_reader.text_sensor";
+        static const char *TAG = "custom_rfid_reader.text_sensor";
 
-void CustomRFIDReader::setup() {
+        CustomRFIDReader::CustomRFIDReader(UARTComponent *parent)
+        {
+        }
 
-}
+        void CustomRFIDReader::setup()
+        {
+        }
 
-void CustomRFIDReader::loop() {
-    ESP_LOGI(TAG, "hey there");
-}
+        int readline(int readch, char *buffer, int len)
+        {
+            static int pos = 0;
+            int rpos;
 
-void CustomRFIDReader::dump_config(){
-    ESP_LOGCONFIG(TAG, "Empty UART sensor");
-}
+            if (readch > 0)
+            {
+                switch (readch)
+                {
+                case '\n':
+                case '\r':           // Return on CR or newline
+                    buffer[pos] = 0; // Just to be sure, set last character 0
+                    rpos = pos;
+                    pos = 0; // Reset position index ready for next time
+                    return rpos;
+                default:
+                    if ((pos < len - 1) && (readch < 127))
+                    { // Filter on <127 to make sure it is a character
+                        buffer[pos++] = readch;
+                        buffer[pos] = 0;
+                    }
+                    else
+                    {
+                        buffer[pos] = 0; // Just to be sure, set last character 0
+                        rpos = pos;
+                        pos = 0; // Reset position index ready for next time
+                        return rpos;
+                    }
+                }
+            }
+            // No end of line has been found, so return -1.
+            return -1;
+        }
 
-}  // namespace empty_UART_sensor
-}  // namespace esphome
+        void CustomRFIDReader::loop()
+        {
+            const int max_line_length = 80;
+            static char buffer[max_line_length];
+            while (available())
+            {
+                if (readline(read(), buffer, max_line_length) > 0)
+                {
+                    publish_state(buffer);
+                }
+            }
+        }
+
+        void CustomRFIDReader::dump_config()
+        {
+            ESP_LOGCONFIG(TAG, "Custom RFID Reader");
+        }
+
+    } // namespace empty_UART_sensor
+} // namespace esphome
--- esphome/components/custom_rfid_reader/custom_rfid_reader.h
+++ esphome/components/custom_rfid_reader/custom_rfid_reader.h
@@ -4,15 +4,20 @@
 #include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/components/uart/uart.h"
 
-namespace esphome {
-namespace custom_rfid_reader {
+namespace esphome
+{
+  namespace custom_rfid_reader
+  {
 
-class CustomRFIDReader : public text_sensor::TextSensor, public Component, public uart::UARTDevice {
- public:
-  void setup() override;
-  void loop() override;
-  void dump_config() override;
-};
+    class CustomRFIDReader : public text_sensor::TextSensor, public Component, public uart::UARTDevice
+    {
+    public:
+      CustomRFIDReader(UARTComponent *parent) : UARTDevice(parent);
 
-}  // namespace empty_uart_sensor
-}  // namespace esphome
+      void setup() override;
+      void loop() override;
+      void dump_config() override;
+    };
+
+  } // namespace empty_uart_sensor
+} // namespace esphome
